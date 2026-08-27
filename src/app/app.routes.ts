8db1a0fefd4548363f162c481473b7eaf4c360bc
raniazerr <rania0270@outlook.fr>
2026-08-27 14:54:08 +0200

    Revert "Merge pull request #2 from raniazerr/feat---home"
    This reverts commit 3dfec96913f369e25abccf2e1d608fd25d6f5e90, reversing
    changes made to 9083b8179f2bd90a428c85ff09b2c0d52c675713.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
 
-export const routes: Routes = [
-  { path: 'home', component: Home },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-];
+export const routes: Routes = [];
