23a5c8cf2685e213f6f78bd940b865dae78cbf44
NolanLefebvre <nolan.lefebvre69@gmail.com>
2026-08-27 15:11:29 +0200

    route about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
-
+import { About } from './pages/about/about';
 export const routes: Routes = [
   { path: 'home', component: Home },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {path:'about', component: About }
 ];
