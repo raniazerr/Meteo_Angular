fa58b5687294e531bf88feed1dd6a7180a6e7e4a
bwm <bw.mouzda@gmail.com>
2026-08-27 16:44:16 +0200

    feat: raccordement de la recherche et de la météo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,25 @@
 import { Routes } from '@angular/router';
+
 import { Home } from './pages/home/home';
+import { Weather } from './components/weather/weather';
 import { About } from './pages/about/about';
+
 export const routes: Routes = [
-  { path: 'home', component: Home },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  {path:'about', component: About }
+  {
+    path: 'home',
+    component: Home
+  },
+  {
+    path: 'weather/:city',
+    component: Weather
+  },
+  {
+    path: 'about',
+    component: About
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
 ];
