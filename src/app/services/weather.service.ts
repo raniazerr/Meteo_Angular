ce58c001cfe894b82c16eff4e7e210ee34df9beb
NolanLefebvre <nolan.lefebvre69@gmail.com>
2026-08-27 15:29:40 +0200

    fix environement

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Weather } from '../models/weather';
 import { map, Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
