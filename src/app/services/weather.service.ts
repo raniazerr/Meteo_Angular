159d18b823986f844b60d8932bd712c222c5defd
NolanLefebvre <nolan.lefebvre69@gmail.com>
2026-08-27 14:44:45 +0200

    creation service + model weather

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Weather } from '../models/weather';
+import { map, Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) {}
+
+  private apiKey = environment.API_KEY;
+
+  getWeather(city: string): Observable<Weather> {
+    return this.http.get<any>(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&lang=fr&units=metric`)
+      .pipe(
+        map(res => ({
+          city: res.name,
+          country: res.sys.country,
+          temperature: res.main.temp,
+          feelsLike: res.main.feels_like,
+          description: res.weather[0].description,
+          humidity: res.main.humidity,
+          windSpeed: res.wind.speed,
+          icon: res.weather[0].icon
+        }))
+      );
+  }
+}
